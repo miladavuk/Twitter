2357639d18e634a3fe9a6d83f1966716ab0fc1b6
miladavuk <milada.vukovic@gmail.com>
2016-04-02 18:59:10 +0200

    Napisana javadoc dokumentacija za klasu TwitterPoruka, njene atribute i metode

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,31 +1,55 @@
 package com.twitter.poruke;
-
+/**
+ * klasa TwitterPoruka
+ * @author Milada Vukovic
+ *
+ */
 public class TwitterPoruka {
-	
+	/**
+	 * atribut <b>koisnik</b> u kom cuvamo ime korisnika
+	 */
 	private String korisnik;
+	/**
+	 * atribut <b>poruka</b> u kom cuvamo sadrzaj poruke
+	 */
 	private String poruka;
-	
+	/**
+	 * metoda koja vraca ime korisnika
+	 * @return korisnik
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
+	/**
+	 * metoda koja postavlja ime korisnika na vrednost parametra
+	 * @param korisnik
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik==null || korisnik == "")
 		throw new RuntimeException(
 		"Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
-	
+	/**
+	 * metoda koja vraca sadrzaj poruke
+	 * @return poruka
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
-	
+	/**
+	 * metoda koja postavlja sadrzaj poruke na vrednost parametra
+	 * @param poruka
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka==null || this.poruka.length()>140)
 		throw new RuntimeException(
 		"Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
-	
+	/**
+	 * redefinisana toString metoda koja vraca String sa atributima klase 
+	 */
 	public String toString(){
 		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	}
